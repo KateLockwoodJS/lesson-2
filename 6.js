7ece789f87c445ae62e5bd44535f39ec87908247
Kate Lockwood <55763653+KateLockwoodJS@users.noreply.github.com>
2019-09-25 15:47:33 +0300

    Update 6.js

--- 6.js
+++ 6.js
@@ -1,24 +1,5 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf500
-{\fonttbl\f0\fswiss\fcharset0 ArialMT;\f1\froman\fcharset0 Times-Roman;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl400\partightenfactor0
+const array = [1, 2, 3, 4];
 
-\f0\fs29\fsmilli14667 \cf2 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 const array = [1, 2, 3, 4];
-\f1\fs24 \
-\pard\pardeftab720\sl280\partightenfactor0
-\cf2 \
-\pard\pardeftab720\sl400\partightenfactor0
-
-\f0\fs29\fsmilli14667 \cf2 for (let i = 0, i < array.length; i++) \{
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 console.log(array[i]);
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 \}
-\f1\fs24 \
+for (let i = 0, i < array.length; i++) {
+console.log(array[i]);
 }
