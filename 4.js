138c610eb42d6eb5fa3f8445a5edb5eb66656c94
Kate Lockwood <55763653+KateLockwoodJS@users.noreply.github.com>
2019-09-25 15:45:32 +0300

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,57 +1,16 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf500
-{\fonttbl\f0\fswiss\fcharset0 ArialMT;\f1\froman\fcharset0 Times-Roman;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl400\partightenfactor0
+const value = 0;
 
-\f0\fs29\fsmilli14667 \cf2 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 const value = 0;
-\f1\fs24 \
-\pard\pardeftab720\sl280\partightenfactor0
-\cf2 \
-\pard\pardeftab720\sl400\partightenfactor0
+switch (value) {
+case 0:
+console.log(0);
+break;
 
-\f0\fs29\fsmilli14667 \cf2 switch (value) \{
-\f1\fs24 \
+case 1:
+console.log(1);
+break;
 
-\f0\fs29\fsmilli14667 case 0:
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 console.log(0);
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 break;
-\f1\fs24 \
-\pard\pardeftab720\sl280\partightenfactor0
-\cf2 \
-\pard\pardeftab720\sl400\partightenfactor0
-
-\f0\fs29\fsmilli14667 \cf2 case 1:
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 console.log(1);
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 break;
-\f1\fs24 \
-\pard\pardeftab720\sl280\partightenfactor0
-\cf2 \
-\pard\pardeftab720\sl400\partightenfactor0
-
-\f0\fs29\fsmilli14667 \cf2 case 2:
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 case 3:
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 console.log(\'912,3\'92);
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 break;
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 \}
-\f1\fs24 \
+case 2:
+case 3:
+console.log(‘2,3’);
+break;
 }
