7783932d544e5a6a6a794d89782483d3ed6b7374
Kate Lockwood <55763653+KateLockwoodJS@users.noreply.github.com>
2019-09-25 15:50:58 +0300

    Update 13.js

--- 13.js
+++ 13.js
@@ -1,36 +1,9 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf500
-{\fonttbl\f0\fswiss\fcharset0 ArialMT;\f1\froman\fcharset0 Times-Roman;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl400\partightenfactor0
+var n = 1000
+var num = 0
 
-\f0\fs29\fsmilli14667 \cf2 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 var n = 1000
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 var num = 0
-\f1\fs24 \
-\pard\pardeftab720\sl280\partightenfactor0
-\cf2 \
-\pard\pardeftab720\sl400\partightenfactor0
-
-\f0\fs29\fsmilli14667 \cf2 while ( n > 50) \{
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 	n = n\'a0 / 2;
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 	num++;
-\f1\fs24 \
-
-\f0\fs29\fsmilli14667 \}
-\f1\fs24 \
-\pard\pardeftab720\sl280\partightenfactor0
-\cf2 \
-\pard\pardeftab720\sl400\partightenfactor0
-
-\f0\fs29\fsmilli14667 \cf2 console.log(num);
-\f1\fs24 \
+while ( n > 50) {
+	n = n  / 2;
+	num++;
 }
+
+console.log(num);
